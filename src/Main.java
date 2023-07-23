21c94bc66321f7e9954b4e2f1e900ba16c057991
Expo <crdailey01@gmail.com>
2023-07-22 22:14:34 -0700

    done

--- src/Main.java
+++ src/Main.java
@@ -54,20 +54,50 @@ public class Main {
 //        System.out.println(sameString.charAt(0)); // '['
 //
 //        System.out.println(sameString.replace(',','*'));
-        String sup = "suspicious";
-        String hi = "hi";
-        System.out.println(sup.equals(hi)); //contents not address
-        System.out.println(sup.indexOf('s')); //returns indeex of first occurence
-        char[] supArray = sup.toCharArray();
-        System.out.println(sup.substring(2, sup.length()-1));
-        System.out.println(sup.contains("ouss"));
-        String spaces = "        me ep       ";
-        System.out.println(spaces);
-        System.out.println(spaces.trim());
-        System.out.println(spaces.replace(" ",""));
 
-        Map<Integer,String> derp = new HashMap<>();
-        derp.
+//        String sup = "suspicious";
+//        String hi = "hi";
+//        System.out.println(sup.equals(hi)); //contents not address
+//        System.out.println(sup.indexOf('s')); //returns indeex of first occurence
+//        char[] supArray = sup.toCharArray();
+//        System.out.println(sup.substring(2, sup.length()-1));
+//        System.out.println(sup.contains("ouss"));
+//        String spaces = "        me ep       ";
+//        System.out.println(spaces);
+//        System.out.println(spaces.trim());
+//        System.out.println(spaces.replace(" ",""));
+
+        Map<Integer, String> derp = new HashMap<>();
+        derp.put(1, "a");
+        derp.put(2, "b");
+        derp.put(3, "c");
+        derp.remove(3);
+        System.out.println(derp.get(2));
+        System.out.println(derp.keySet());
+        System.out.println(derp.toString());
+
+        Map<Integer, String> herp = new HashMap<>();
+        herp.put(1, "a");
+        herp.put(2, "b");
+        System.out.println(derp.equals(herp));
+        herp.replace(2, "q");
+        System.out.println(herp.get(2));
+
+
+        Integer number = 10;
+        String ten = number.toString();
+        System.out.println(ten);
+        System.out.println(ten.replaceFirst("1", "") + " replaced");
+
+        StringBuilder sb = new StringBuilder("hi");
+        System.out.println(sb);
+        sb.append("ho");
+        System.out.println(sb);
+        sb.deleteCharAt(2);
+        sb.reverse();
+        System.out.println(sb.charAt(0));
+        System.out.println(sb.substring(1, 2));
+
 
         /*
         array: .equals(), .toString(), .length --> instance methods
@@ -88,10 +118,15 @@ public class Main {
            LinkedList: addFirst, removeFirst, addLast, removeLast, add, remove, get, set, size, contains, pop,
             peek() returns head, peekLast() returns tail, push, pop
 
+
+            Map: put, remove, get, equals() based on contents, replace, containsKey, containsValue, size, isEmpty, toString, keySet
+
+            Integer: toString
+
+            StringBuilder: append(""), length, deleteCharAt, delete, reverse, charAt, subString, isEmpty
          */
 
     }
 }
-// Map
-// Integer
+
 // StringBuilder
